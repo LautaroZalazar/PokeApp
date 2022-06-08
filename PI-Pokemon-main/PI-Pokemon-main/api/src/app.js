07a5a4a76494bbe4ff177449e935f059413ec6f4
Lautaro Zalazar <lautyzalazar10@gmail.com>
2022-06-08 12:27:19 -0300

    Deploy

--- PI-Pokemon-main/PI-Pokemon-main/api/src/app.js
+++ PI-Pokemon-main/PI-Pokemon-main/api/src/app.js
@@ -3,9 +3,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require('cors')
 
 require('./db.js');
+const cors = require('cors')
 
 const server = express();
 
